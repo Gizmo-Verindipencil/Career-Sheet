ef1b4a3a25cb9d767ef1d24f4343b8a9d2f15fd6
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2021-03-27 15:53:24 +0900

    Add help information

--- js/repository/other-repository.js
+++ js/repository/other-repository.js
@@ -42,6 +42,21 @@ class OtherRepository {
                 "id" : "7",
                 "name" : "JQuery formBuilder",
                 "description" : "Kevin Chappell氏が提供するJQueryプラグイン。ドラッグ&ドロップでのWEB画面を作成する機能を提供。"
+            },
+            {
+                "id" : "8",
+                "name" : "Packery",
+                "description" : "David DaSandro氏が提供するJQueryプラグイン。ドラッグ&ドロップでの動かせる隙間のないWEBレイアウト(瓶詰めレイアウト)を実現できる。"
+            },
+            {
+                "id" : "9",
+                "name" : "shopify draggable",
+                "description" : "Spofy氏が提供するJavaScriptライブラリ。独自のドラッグ&ドロップを拡張するのに役立つ。2021年3月時点では、更新が止まっている。"
+            },
+            {
+                "id" : "10",
+                "name" : "interact.js",
+                "description" : "taye氏が提供するJavaScriptライブラリ。ドラッグ&ドロップを利用する為のものだが、APIを通してイベント・ポイントが提供される為「より自由度が高い」と謳っている。"
             }
         ];
     }
