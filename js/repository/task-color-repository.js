30f492379fcac16123b4f5c52cb6f83a26a1c2bb
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2021-05-02 15:54:20 +0900

    Change task color

--- js/repository/task-color-repository.js
+++ js/repository/task-color-repository.js
@@ -10,80 +10,30 @@ class TaskColorRepository {
     getAll = () => {
         return [
             {
-                "id" : "DevUP1",
-                "start" : "#3F91EA",
-                "end" : "#0D5FB8"
+                "id" : "DevUP",
+                "start" : "#CCFFCC",
+                "end" : "#7CAF7C"
             },
             {
-                "id" : "DevUP2",
-                "start" : "#4D97DB",
-                "end" : "#1B65A9"
+                "id" : "DevMP",
+                "start" : "#CCECFF",
+                "end" : "#7C9CAF"
             },
             {
-                "id" : "DevUP3",
-                "start" : "#5A9CCB",
-                "end" : "#286A99"
+                "id" : "DevLP",
+                "start" : "#FFFFCC",
+                "end" : "#AFAF7C"
             },
             {
-                "id" : "DevUP4",
-                "start" : "#68A2BC",
-                "end" : "#36708A"
+                "id" : "RW",
+                "start" : "#FCD5B4",
+                "end" : "#AC8564"
             },
             {
-                "id" : "DevUP5",
-                "start" : "#75A7AC",
-                "end" : "#43757A"
-            },
-            {
-                "id" : "DevUP6",
-                "start" : "#83AC9D",
-                "end" : "#517A6B"
-            },
-            {
-                "id" : "DevUP7",
-                "start" : "#90B28D",
-                "end" : "#5E805B"
-            },
-            {
-                "id" : "DevMP1",
-                "start" : "#9EB77E",
-                "end" : "#6C854C"
-            },
-            {
-                "id" : "DevMP2",
-                "start" : "#ABBD6E",
-                "end" : "#798B3C"
-            },
-            {
-                "id" : "DevMP3",
-                "start" : "#B9C25F",
-                "end" : "#87902D"
-            },
-            {
-                "id" : "DevMP4",
-                "start" : "#C6C74F",
-                "end" : "#94951D"
-            },
-            {
-                "id" : "DevLP1",
-                "start" : "#D4CD40",
-                "end" : "#A29B0E"
-            },
-            {
-                "id" : "DevLP2",
-                "start" : "#E1D230",
-                "end" : "#AFA000"
-            },
-            {
-                "id" : "DevLP3",
-                "start" : "#EFD821",
-                "end" : "#BDA600"
-            },
-            {
-                "id" : "DevLP4",
-                "start" : "#FCDD11",
-                "end" : "#CAAB00"
-            }       
+                "id" : "O",
+                "start" : "#D9D9D9",
+                "end" : "#898989"
+            }
         ];
     }
 }
