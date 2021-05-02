e2e3d104d89453e18101713e6835ea63bcbe3c75
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2021-05-02 18:33:32 +0900

    Fix attribute name

--- js/controller/task-type-list-controller.js
+++ js/controller/task-type-list-controller.js
@@ -51,7 +51,7 @@ class TaskTypeListController {
         // 作業種類毎のデザインを適用
         for (const type of types) {
             // 対応する色を設定
-            const theme = this.model.getTaskThemeById(type.colorId);
+            const theme = this.model.getTaskThemeById(type.themeId);
             const createStyle = (foreColor, backgroundColor) => `style='color:${foreColor};background-color:${backgroundColor};'`;
             let style = "";
             if (theme) {
