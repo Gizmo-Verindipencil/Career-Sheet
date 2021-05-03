676cd99b19635b5ec58e42e83b75338d9c6440b7
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2021-05-04 03:41:02 +0900

    Add task type sorting of work experience

--- js/model/work-experience-model.js
+++ js/model/work-experience-model.js
@@ -32,6 +32,41 @@ class WorkExperienceModel {
     }
 
     /**
+     * ソート済の作業種類データの識別子を取得します。
+     * @param {Array<String>} taskTypeIds 作業種類データの識別子。
+     * @return {Array<String>} ソート済の作業種類データの識別子を返します。対応する作業種類データがない識別子は、末尾に配置されます。
+     */
+    getSortedTaskTypeIds = taskTypeIds => {
+        // 作業種類データを取得
+        const repository = new TaskTypeRepository();
+        const cachedTypes = repository.getAll();
+
+        // ソートの最大を取得
+        const maxSort = Math.max(...cachedTypes.map(x => Number(x.sort)));
+
+        console.log(maxSort);
+        // 作業種類データのソートを設定
+        const sortOrder = [];
+        for(const id of taskTypeIds) {
+            const types = cachedTypes.filter(x => x.id === id);
+            
+            // 対応するデータが無ければ最後に表示
+            sortOrder.push({
+                id: id,
+                sort: types.length > 0 ? types[0] : maxSort + 1
+            });
+        }
+
+        // ソートの昇順で並べる
+        sortOrder.sort((a, b) => {
+            return a.sort > b.sort
+        });
+
+        // 識別子の配列で返す
+        return sortOrder.map(x => x.id);
+    }
+
+    /**
      * 作業テーマデータを取得します。
      * @param {String} taskTypeId 作業種類データの識別子。
      * @returns {Object} 作業テーマデータを返します。該当データがない場合、nullを返します。
