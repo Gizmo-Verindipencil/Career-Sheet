9b93bfc098c311da24739128d7f513dc605a74dc
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2021-06-09 23:08:01 +0900

    Add controller, model, and seasonal design for the page of personal interest

---
+++ js/controller/supplementary/personal-interest-controller.js
@@ -0,0 +1,55 @@
+import { PersonalInterestModel } from "../../model/supplementary/personal-interest-model.min.js";
+import { Utility } from "../../shared/utility.min.js";
+import ScriptSeriesLoader from "../../shared/script-series-loader.min.js"
+
+/**
+ * 個人的な関心ページのコントローラーを提供します。
+ */
+class PersonalInterestController {
+    /**
+     * インスタンスを初期化します。
+     */
+    constructor() {
+        // 対応するモデルをセット
+        this.model = new PersonalInterestModel();
+
+        // 必要なスクリプトを読込
+        this.scriptLoader = ScriptSeriesLoader;
+        this.scriptLoader.add("js/vendor/season-reminder.min.js");
+        this.scriptLoader.load();
+    }
+
+    /**
+     * インスタンスの生成し、必要なモジュールを読込します。
+     * @returns {PersonalInterestController} 新しいインスタンスを返します。
+     */
+    static build = async() => {
+        // インスタンスを作成
+        const controller = new PersonalInterestController();
+
+        // スクリプトの読込完了後にインスタンスを返す
+        while(controller.scriptLoader.running){
+            await Utility.sleep(2000);
+        }
+        return controller;
+    }
+
+    /**
+     * 個人的な関心ページの設定を実行します。
+     */
+    execute = () => {
+        // 色を調整
+        this.changeBackgroundColor();
+    }
+
+    /**
+     * 背景色を季節を反映した内容に変える。
+     */
+    changeBackgroundColor = () => {
+        const reminder = new SeasonReminder();
+        reminder.seasonInfluence = 10;
+        reminder.remindAll("background-color");
+    }
+}
+
+export { PersonalInterestController };
